ec2e58117770e455df1e8d2412edaccd67796299
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-06 15:20:17 -0800

    add tqdm

--- get_min_max_intensity.py
+++ get_min_max_intensity.py
@@ -1,6 +1,7 @@
 import os
 import nibabel as nib
 import numpy as np
+from tqdm import tqdm
 
 root_dir = "/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2"
 
@@ -8,7 +9,7 @@ global_min = float('inf')
 global_max = float('-inf')
 
 # Recursively traverse the directory
-for root, dirs, files in os.walk(root_dir):
+for root, dirs, files in tqdm(os.walk(root_dir)):
     for fname in files:
         # Check for .nii.gz extension and skip if 'seg' is in the filename
         if fname.endswith(".nii.gz") and "seg" not in fname.lower():
