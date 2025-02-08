e470ecbadcef8462c0f20cdce8ea9ff945c1c8b1
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-08 03:00:58 -0800

    reduce batch size

--- LaMed/script/finetune_brats_lora_phi3.sh
+++ LaMed/script/finetune_brats_lora_phi3.sh
@@ -15,7 +15,7 @@ accelerate launch --gpu_ids $1 LaMed/src/train/train.py \
     --bf16 True \
     --output_dir ./LaMed/output/LaMed-Phi3-4B-finetune-0000 \
     --num_train_epochs 5 \
-    --per_device_train_batch_size 8 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
