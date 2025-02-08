df2210576b303bf2e545a97ef4b9bba1ba75daaa
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-08 02:48:51 -0800

    debug

--- LaMed/script/finetune_brats_lora_phi3.sh
+++ LaMed/script/finetune_brats_lora_phi3.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # run "accelerate config" first!
-export CUDA_VISIBLE_DEVICES=$1
-accelerate launch LaMed/src/train/train.py \
+accelerate launch --gpu_ids $1 LaMed/src/train/train.py \
     --version v0 \
     --model_name_or_path microsoft/Phi-3-mini-4k-instruct \
     --model_type phi3 \
