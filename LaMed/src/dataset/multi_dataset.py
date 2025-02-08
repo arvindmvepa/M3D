c6cf7d8c2fb5204b1877d71c4131600a5e4db546
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-08 03:17:34 -0800

    test using multiple modalities

--- LaMed/src/dataset/multi_dataset.py
+++ LaMed/src/dataset/multi_dataset.py
@@ -371,12 +371,14 @@ class VQABratsDataset(VQADataset):
         return data_list
 
     def __getitem__(self, idx):
-        # TODO: Figure out how to use more modalities
         data = self.data_list[idx]
 
-        image_abs_path = data["volume_non_seg_files"]["t1c"]
-        new_image_abs_path = self.convert_file_path_to_npy(image_abs_path)
-        image = np.load(new_image_abs_path)
+        image = []
+        for modality in ["t1c", "t1n", "t2f", "t2w"]:
+            image_abs_path = data["volume_non_seg_files"][modality]
+            new_image_abs_path = self.convert_file_path_to_npy(image_abs_path)
+            image.append(np.load(new_image_abs_path))
+        image = np.concatenate(image, axis=0)
 
         image = self.transform(image)
 
