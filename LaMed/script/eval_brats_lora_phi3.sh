7aab9658c1abe2ff8262cd9b065190287f1d17cd
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-10 15:50:31 -0800

    bug

--- LaMed/script/eval_brats_lora_phi3.sh
+++ LaMed/script/eval_brats_lora_phi3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES="" python Bench/eval/eval_TASK.py \
+CUDA_VISIBLE_DEVICES="" python Bench/eval/eval_vqa.py \
 --model_name_or_path LaMed/output/LaMed-Phi3-4B-finetune-0000/hf \
 --vqa_data_test_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v2.json \
