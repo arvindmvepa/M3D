5219a2a09ee4bdcdc9a763bb250b316cebd95389
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-22 04:41:26 -0800

    MET data download

--- prepare_brats_dataset.py
+++ prepare_brats_dataset.py
@@ -13,8 +13,10 @@ import nibabel as nib
 
 #input_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2'
 #output_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2_npy'
-input_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth'
-output_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth_npy'
+#input_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth'
+#output_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth_npy'
+input_dir = '/local2/shared_data/BraTS2024-BraTS-MET/MICCAI-BraTS2024-MET-Challenge-Training_overall'
+output_dir = '/local2/shared_data/BraTS2024-BraTS-MET/MICCAI-BraTS2024-MET-Challenge-Training_overall_npy'
 
 # Get all subfolders [00001, 00002....]
 subfolders = [folder for folder in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, folder))]
