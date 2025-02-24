0da0602e8e80d75cbb5e5654dbd395bef015a72b
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-23 17:57:05 -0800

    update train script with updated files

--- LaMed/script/finetune_brats_lora_phi3_freeze_viz.sh
+++ LaMed/script/finetune_brats_lora_phi3_freeze_viz.sh
@@ -6,9 +6,9 @@ accelerate launch --gpu_ids $1 --main_process_port 29600 LaMed/src/train/train.p
     --version v0 \
     --model_name_or_path microsoft/Phi-3-mini-4k-instruct \
     --model_type phi3 \
-    --vqa_data_train_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_v2.json \
-    --vqa_data_val_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_v2.json \
-    --vqa_data_test_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v2.json \
+    --vqa_data_train_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_v3.json \
+    --vqa_data_val_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_v3.json \
+    --vqa_data_test_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v3.json \
     --lora_enable True \
     --vision_tower vit3d \
     --pretrain_vision_model /local2/amvepa91/M3D/LaMed/pretrained_model/M3D-CLIP/pretrained_ViT.bin \
