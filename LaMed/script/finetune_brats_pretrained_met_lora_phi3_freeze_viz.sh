ecbfab11d782b745e44d2442eb910e81c872762d
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-24 16:02:00 -0800

    try to use pretrained weights for training

---
+++ LaMed/script/finetune_brats_pretrained_met_lora_phi3_freeze_viz.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# run "accelerate config" first!
+output_dir=./LaMed/output/LaMed-Phi3-4B-finetune-freeze-brats-pretrained-met-viz-0000
+accelerate launch --gpu_ids $1 --main_process_port 29600 LaMed/src/train/train.py \
+    --version v0 \
+    --model_name_or_path microsoft/Phi-3-mini-4k-instruct \
+    --model_type phi3 \
+    --vqa_data_train_path /local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_train_v1.json \
+    --vqa_data_val_path /local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_val_v1.json \
+    --vqa_data_test_path /local2/amvepa91/MedTrinity-25M/brats_met_3d_vqa_subjTrue_test_v1.json \
+    --lora_enable True \
+    --vision_tower vit3d \
+    --pretrain_vision_model /local2/amvepa91/M3D/LaMed/pretrained_model/M3D-CLIP/pretrained_ViT.bin \
+    --pretrain_mm_mlp_adapter /local2/amvepa91/M3D/LaMed/pretrained_model/M3D-LaMed-Phi-3-4B/mm_projector.bin \
+    --pretrain_mllm /local2/amvepa91/M3D/LaMed/output/LaMed-Phi3-4B-finetune-freeze-viz-0000/hf \
+    --freeze_vision_tower True \
+    --bf16 True \
+    --output_dir $output_dir \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --evaluation_strategy "steps" \
+    --eval_accumulation_steps 1 \
+    --eval_steps 0.04 \
+    --save_strategy "steps" \
+    --save_steps 1000 \
+    --save_total_limit 1 \
+    --learning_rate 5e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 0.001 \
+    --gradient_checkpointing False \
+    --dataloader_pin_memory True\
+    --dataloader_num_workers 8 \
+    --report_to tensorboard
+
+PYTHONPATH=. CUDA_VISIBLE_DEVICES="" python LaMed/src/utils/merge_lora_weights_and_save_hf_model.py \
+--version="" --model_type="phi3" \
+--model_with_lora="$output_dir"/model_with_lora.bin \
+--output_dir="$output_dir"/hf
+
+PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python Bench/eval/eval_vqa.py \
+--model_name_or_path "$output_dir"/hf \
+--vqa_data_test_path /local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v2.json \
+--output_dir $output_dir/eval_vqa/
+
--- LaMed/src/train/train.py
+++ LaMed/src/train/train.py
@@ -268,6 +268,7 @@ def main():
     local_rank = training_args.local_rank
 
     rank0_print("="*20 + " Tokenizer preparation " + "="*20)
+
     # Load tokenizer from the given path with specified configurations
     tokenizer = AutoTokenizer.from_pretrained(
         model_args.model_name_or_path,
@@ -296,9 +297,27 @@ def main():
     model_args.vocab_size = len(tokenizer)
     rank0_print("seg_token_id: ", model_args.seg_token_id)
     rank0_print("vocab_size: ", model_args.vocab_size)
-
     rank0_print("="*20 + " Model preparation " + "="*20)
-    if model_args.vision_tower is not None:
+    if os.path.exists(model_args.model_name_or_path):
+        """
+        print(f"loading existing model {model_args.model_name_or_path}")
+        if 'llama' in model_args.model_name_or_path.lower():
+            model = LamedLlamaForCausalLM.from_pretrained(
+                model_args.model_name_or_path,
+                cache_dir=training_args.cache_dir,
+                bos_token_id=tokenizer.bos_token_id,
+                eos_token_id=tokenizer.eos_token_id,
+            )
+        elif 'phi3' in model_args.model_name_or_path.lower():
+            model = LamedPhi3ForCausalLM.from_pretrained(
+                model_args.model_name_or_path,
+                cache_dir=training_args.cache_dir
+            )
+        else:
+            raise ValueError(f"Unknown Model Type {model_args.model_name_or_path}")
+        """
+        pass
+    elif model_args.vision_tower is not None:
         if 'llama' in model_args.model_type:
             model = LamedLlamaForCausalLM.from_pretrained(
                 model_args.model_name_or_path,
