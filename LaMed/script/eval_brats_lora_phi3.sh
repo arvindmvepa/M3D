c48833842630c0bb1a40df34516c015bf7757938
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-02-24 23:15:09 -0800

    bug fix

--- LaMed/script/eval_brats_lora_phi3.sh
+++ LaMed/script/eval_brats_lora_phi3.sh
@@ -6,5 +6,4 @@ test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_test_v3.json
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python Bench/eval/eval_vqa.py \
 --model_name_or_path $output_dir/hf \
 --vqa_data_test_path $test_path \
-
-
+--output_dir $output_dir/eval_vqa/
