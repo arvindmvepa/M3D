775f7eb1ae50402f480e64ac4eea56ff41e54ca7
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-03-28 02:00:36 -0700

    add .10 multiplier to distance_aware_bce_loss

--- Bench/eval/train_brats_aux_tasks.py
+++ Bench/eval/train_brats_aux_tasks.py
@@ -186,7 +186,8 @@ def compute_aux_loss(
     solidity_loss = coral_loss(solidity_2d, solidity_tgt_1d, K_solidity)
 
     if dist_bbox_loss:
-        bbox_loss = distance_aware_bce_loss(bbox_logits, bbox_targets)
+        # make sure to add a .10 multiplier because of large loss size
+        bbox_loss = .10 * distance_aware_bce_loss(bbox_logits, bbox_targets)
     else:
         bbox_loss = soft_jaccard_loss(bbox_logits, bbox_targets)
 
