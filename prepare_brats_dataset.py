6f4a49c67aa014c4293c9f79a34be10d0043ea6b
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-04-03 13:09:21 -0700

    add goat/peds dataset

--- prepare_brats_dataset.py
+++ prepare_brats_dataset.py
@@ -11,8 +11,14 @@ from unidecode import unidecode
 import nibabel as nib
 
 
-input_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2'
-output_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2_npy'
+#input_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2'
+#output_dir = '/local2/shared_data/BraTS2024-BraTS-GLI/training_data1_v2_npy'
+#input_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth'
+#output_dir = '/local2/shared_data/BraTS2024-BraTS-GoAT/MICCAI2024-BraTS-GoAT-TrainingData-With-GroundTruth_npy'
+#input_dir = '/local2/shared_data/BraTS2024-BraTS-MET/MICCAI-BraTS2024-MET-Challenge-Training_overall'
+#output_dir = '/local2/shared_data/BraTS2024-BraTS-MET/MICCAI-BraTS2024-MET-Challenge-Training_overall_npy'
+input_dir = '/local2/shared_data/BraTS-PEDs2024/BraTS-PEDs2024_Training'
+output_dir = '/local2/shared_data/BraTS-PEDs2024/BraTS-PEDs2024_Training_npy'
 
 # Get all subfolders [00001, 00002....]
 subfolders = [folder for folder in os.listdir(input_dir) if os.path.isdir(os.path.join(input_dir, folder))]
@@ -55,7 +61,7 @@ def process_subfolder(subfolder):
 
             np.save(output_path, img_trans)
         except:
-            print([img.shape for img in images_3d])
+            print([img.shape for img in img_array])
             print("This folder is vstack error: ", output_path)
 
 
