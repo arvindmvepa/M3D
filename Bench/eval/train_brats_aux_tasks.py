24d1bec88a616b6df1e6f1f2e89afddeed60d970
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-06-04 03:33:38 -0700

    bug fix

--- Bench/eval/train_brats_aux_tasks.py
+++ Bench/eval/train_brats_aux_tasks.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 import math
+import collections
 
 from torch.utils.data import DataLoader, Dataset
 from tqdm import tqdm
