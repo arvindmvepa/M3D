35892a725aa00b7588bea391559aa1bdc0e6f2e5
Arvind Vepa <arvind.m.vepa@gmail.com>
2025-10-17 19:56:40 -0700

    bug fix - also test evaluate

--- Bench/eval/train_nlst_aux_tasks.py
+++ Bench/eval/train_nlst_aux_tasks.py
@@ -367,9 +367,9 @@ def main():
     val_dataset = AuxVisionDataset(val_file, mode="val")
     test_dataset = AuxVisionDataset(test_file, mode="test")
 
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
-    val_loader = DataLoader(val_dataset,   batch_size=args.batch_size, shuffle=False)
-    test_loader = DataLoader(test_dataset,  batch_size=args.batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
+    val_loader = DataLoader(val_dataset,   batch_size=args.batch_size, shuffle=False, drop_last=True)
+    test_loader = DataLoader(test_dataset,  batch_size=args.batch_size, shuffle=False, drop_last=True)
 
     logger.info(f"Dataset sizes => train={len(train_dataset)}, val={len(val_dataset)}, test={len(test_dataset)}")
 
