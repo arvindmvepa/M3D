8a796dca80763efca7115c1854c09c857eb27596
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-19 02:48:33 -0800

    do inference on the larger gli dataset

--- LaMed/script/clinical_eval_w_llava_med_new_dataset.sh
+++ LaMed/script/clinical_eval_w_llava_med_new_dataset.sh
@@ -4,13 +4,13 @@
 output_dir=./LaMed/output/LaMed-Phi3-4B-multimodal-combined-finetune-freeze-viz-again-new-dataset-v11-0000
 train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
 val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
-test_path=/local2/amvepa91/clinical_validation_test_set.json
+test_path=/local2/amvepa91/clinical_validation_gli_test_set.json
 
-#PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python Bench/eval/eval_vqa.py \
-#--output_dir $output_dir/eval_vqa1 \
-#--model_name_or_path "$output_dir"/hf \
-#--vqa_data_test_path $test_path \
+PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python Bench/eval/eval_vqa.py \
+--output_dir $output_dir/eval_vqa2 \
+--model_name_or_path "$output_dir"/hf \
+--vqa_data_test_path $test_path \
 
 PYTHONPATH=. CUDA_VISIBLE_DEVICES=$1 python Bench/eval/eval_vqa_utils.py \
---output_dir $output_dir/eval_vqa1 \
+--output_dir $output_dir/eval_vqa2 \
 --gt_file $test_path \
