936949ac31060b08569bda323cce1c8f9915e45c
Arvind Murari Vepa <arvind.m.vepa@gmail.com>
2025-11-24 00:52:39 -0800

    bug fix

--- LaMed/script/train_jobs_w_m3d_and_llava_med_new_dataset_gli_met.sh
+++ LaMed/script/train_jobs_w_m3d_and_llava_med_new_dataset_gli_met.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # m3d job
-#output_dir=./LaMed/output/LaMed-Phi3-4B-multimodal-combined-finetune-freeze-viz-again-new-dataset-gli_met_v11-0000
+output_dir=./LaMed/output/LaMed-Phi3-4B-multimodal-combined-finetune-freeze-viz-again-new-dataset-gli_met_v11-0000
 #train_path=/local2/amvepa91/MedTrinity-25M/brats_gli_met_3d_vqa_subjTrue_train_updated_v11_seed0_multitask_fixed.json
 #val_path=/local2/amvepa91/MedTrinity-25M/brats_gli_met_3d_vqa_subjTrue_val_updated_v11_seed0_multitask_fixed.json
 test_path=/local2/amvepa91/MedTrinity-25M/brats_gli_met_3d_vqa_subjTrue_test_updated_v11_seed0.json
